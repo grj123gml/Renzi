bd322f5c620bc365162809f936e4e537b9ea56c6
grj <3303272844@qq.com>
2022-08-14 14:55:24 +0800

    完成员工的导入导出功能

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <upload-excel :beforeUpload="beforeUpload"></upload-excel>
+      <upload-excel
+        :beforeUpload="excelSuccess"
+        :onSuccess="onSuccess"
+      ></upload-excel>
     </div>
   </div>
 </template>
 
 <script>
+import { formatTime } from '@/filters'
+import { importEmployee } from '@/api/employees'
+import employess from '@/constant/employees'
+const { importmapKeyPath } = employess
 export default {
   data() {
     return {}
@@ -15,7 +22,37 @@ export default {
   created() {},
 
   methods: {
-    beforeUpload() {}
+    // 限制上传前文件类型
+    excelSuccess({ name }) {
+      if (!name.endsWith('.xlsx')) {
+        this.$message.error('请上传xlsx文件')
+        return false
+      }
+      return true
+    },
+    // 上传成功后
+    async onSuccess({ header, results }) {
+      // console.log(header)
+      // console.log(results)
+      const newArr = results.map((item) => {
+        const obj = {}
+        for (let key in importmapKeyPath) {
+          if (key === '入职日期' || key === '转正日期') {
+            const timestamp = item[key]
+            const data = new Date((timestamp - 1) * 24 * 60 * 60 * 1000)
+            data.setFullYear(data.getFullYear() - 70)
+            obj[importmapKeyPath[key]] = formatTime(data)
+          } else {
+            obj[importmapKeyPath[key]] = item[key]
+          }
+        }
+        return obj
+      })
+      // console.log(newArr)
+      await importEmployee(newArr)
+      this.$message.success('成功上传')
+      this.$router.push('/employees')
+    }
   }
 }
 </script>
